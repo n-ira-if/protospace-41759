93de108119ce4a8c28653ee6dd215d8525ce5684
n-ira-if <ibnoaria@yahoo.co.jp>
2024-11-19 10:55:04 +0900

    再度修正コメント空白時

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,7 +7,7 @@ class CommentsController < ApplicationController
       redirect_to prototype_path(@comment.prototype)
     else
       @prototype = @comment.prototype
-      @comments = @prototype.comment
+      @comments = @prototype.content
       render "prototypes/show", status: :unprocessable_entity
     end
   end
